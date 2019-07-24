d9cdca047211bb2f6d7a5bf1b060c1d5f6e502c2
Scott <swsteffes@gmail.com>
2019-07-24 11:30:29 +0900

    Add demo

---
+++ Assets/Scripts/GoToAttractMode.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GoToAttractMode : MonoBehaviour {
+    public Text timeText;
+    public Text secondsText;
+    public string attractModeSceneName;
+
+    private const int WAIT_TIME = 15;
+    private float lastInteractionTime;
+    private float lastTextChangeTime;
+    private void Start()
+    {
+        lastInteractionTime = Time.time;
+        lastTextChangeTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Input.anyKey)
+        {
+            lastInteractionTime = Time.time;
+            UpdateUIText();
+        }
+        if(Time.time - lastInteractionTime > WAIT_TIME)
+        {
+            SceneManager.LoadScene(attractModeSceneName);
+        }
+        else
+        {
+            if(Time.time - lastTextChangeTime > 1)
+            {
+                UpdateUIText();
+                lastTextChangeTime = Time.time;
+            }
+        }
+    }
+
+    private void UpdateUIText()
+    {
+        int secondsLeft = WAIT_TIME - Mathf.RoundToInt(Time.time - lastInteractionTime);
+        timeText.text = secondsLeft.ToString();
+        secondsText.text = secondsLeft == 1 ? "Second" : "Seconds";
+    }
+}
---
+++ Assets/Scripts/ReturnToMainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ReturnToMainMenu : MonoBehaviour {
+
+    public string mainMenuSceneName;
+
+    public Text demoText;
+
+    private const float FLASHES_PER_SECOND = 1.5f;
+    private static readonly Color FLASH_COLOR = new Color(1, 1, 1, 0.75f);
+    void Update () {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene(mainMenuSceneName);
+        }
+        demoText.color = (Mathf.RoundToInt(Time.time * FLASHES_PER_SECOND) % 2 == 0) ? FLASH_COLOR : Color.clear;
+    }
+}
